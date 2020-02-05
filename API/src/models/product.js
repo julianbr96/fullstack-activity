9a9d42bc397c0a03695a554636cc94ace05a7f53
Julian Baños y Rivas <julian.b@sirena.app>
2020-02-05 19:50:25 -0300

    added product model

---
+++ API/src/models/product.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  inStock: { type: Boolean, required: true }
+});
+
+module.exports = mongoose.model('Product', productSchema);
